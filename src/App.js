88d9dcdb2e3f44b4eaeed0c670438f9f4bbc1790
maruyou0621 <norio_shibata@maru-you.co.jp>
2025-02-19 00:27:37 +0900

    Fix JSX error for img tag

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
     <div>
       {/* A8.net テキスト広告エリア */}
       <div className="ad-container">
-        <a href="https://px.a8.net/svt/ejp?a8mat=44Z1NL+F75DPU+5M44+5YRHE" 
-           rel="nofollow" target="_blank" rel="noopener noreferrer" className="ad-text">
+        <a href="https://px.a8.net/svt/ejp?a8mat=44Z1NL+F75DPU+5M44+5YRHE" rel="nofollow">
           【花西子(フローラシス)】日本でも大人気の中国コスメ
         </a>
+        {/* 🔥 ここを自己閉じタグに修正！ */}
         <img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=44Z1NL+F75DPU+5M44+5YRHE" alt="" />
       </div>
 
