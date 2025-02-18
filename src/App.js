3c52c1bbf8ce96ce1a767507da8935101260fc74
maruyou0621 <norio_shibata@maru-you.co.jp>
2025-02-19 00:19:17 +0900

    Adjust A8.net ad spacing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import HiraganaQuiz from "./components/HiraganaQuiz"; 
-import "./App.css"; // ✅ スタイルを適用するために追加！
+import "./App.css";
 
 function App() {
   return (
     <div>
+      {/* A8.net テキスト広告エリア */}
+      <div className="ad-container">
+        <a href="https://px.a8.net/svt/ejp?a8mat=44Z1NL+F75DPU+5M44+5YRHE" 
+           rel="nofollow" target="_blank" rel="noopener noreferrer" className="ad-text">
+          【花西子(フローラシス)】日本でも大人気の中国コスメ
+        </a>
+        <img border="0" width="1" height="1" src="https://www10.a8.net/0.gif?a8mat=44Z1NL+F75DPU+5M44+5YRHE" alt="" />
+      </div>
+
+      {/* ひらがなクイズ */}
       <HiraganaQuiz />
     </div>
   );
