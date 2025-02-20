cde4e0aafc9c20a6b99324a5301f22f81fcbb4f3
maruyou0621 <norio_shibata@maru-you.co.jp>
2025-02-20 13:38:24 +0900

    Vercelへデプロイ

---
+++ src/components/AdBannerIncorrect.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AdBannerIncorrect = () => {
+  return (
+    <div className="a8-banner incorrect-ad">
+      <a href="https://px.a8.net/svt/ejp?a8mat=44Z2FF+E22GZ6+348+6C1VL" rel="nofollow" target="_blank" rel="noopener noreferrer">
+        <img border="0" width="300" height="300" alt="広告" src="https://www22.a8.net/svt/bgt?aid=250219275850&wid=001&eno=01&mid=s00000000404001064000&mc=1" />
+      </a>
+      <img border="0" width="1" height="1" src="https://www11.a8.net/0.gif?a8mat=44Z2FF+E22GZ6+348+6C1VL" alt="" />
+    </div>
+  );
+};
+
+export default AdBannerIncorrect;
